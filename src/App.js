c9cb19c9de81aae7fb43357409da416aeeff99d7
Frank South <frankstonesouth@gmail.com>
2022-07-07 13:51:35 -0400

    Added skeleton files and removed unnecessary files/comments

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+
+function App() {
+  const name = 'Frank'
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
